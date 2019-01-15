3244bca0aa4601fcd1617ba3345652d45bed4ed4
Zack Bolin <bolinzachary@gmail.com>
2019-01-14 21:57:24 -0800

    Comment unused code.

--- src/Mordor.Process/Mordor.Process/Internal/Extensions.cs
+++ src/Mordor.Process/Mordor.Process/Internal/Extensions.cs
@@ -8,10 +8,10 @@ namespace Mordor.Process.Internal
 {
     internal static class Extensions
     {
-        public static SafeHandle GetSafeHandle(this IntPtr ptr, bool ownsHandle = false, Func<IntPtr, bool> disposer = default)
-        {
-            return new SafePtr(ptr, ownsHandle, disposer);
-        }
+        //public static SafeHandle GetSafeHandle(this IntPtr ptr, bool ownsHandle = false, Func<IntPtr, bool> disposer = default)
+        //{
+        //    return new SafePtr(ptr, ownsHandle, disposer);
+        //}
 
         public static uint GetSize(this Type type)
         {
--- src/Mordor.Process/Mordor.Process/Internal/SafePtr.cs
+++ src/Mordor.Process/Mordor.Process/Internal/SafePtr.cs
@@ -3,20 +3,20 @@ using Microsoft.Win32.SafeHandles;
 
 namespace Mordor.Process.Internal
 {
-    internal sealed class SafePtr : SafeHandleZeroOrMinusOneIsInvalid
-    {
-        private readonly Func<IntPtr, bool> _disposer;
+    //internal sealed class SafePtr : SafeHandleZeroOrMinusOneIsInvalid
+    //{
+    //    private readonly Func<IntPtr, bool> _disposer;
 
-        public SafePtr(IntPtr value, bool ownsHandle = false, Func<IntPtr, bool> disposer = default)
-            :base(ownsHandle)
-        {
-            _disposer = disposer;
-            handle = value;
-        }
+    //    public SafePtr(IntPtr value, bool ownsHandle = false, Func<IntPtr, bool> disposer = default)
+    //        :base(ownsHandle)
+    //    {
+    //        _disposer = disposer;
+    //        handle = value;
+    //    }
 
-        protected override bool ReleaseHandle()
-        {
-            return _disposer?.Invoke(handle) ?? true;
-        }
-    }
+    //    protected override bool ReleaseHandle()
+    //    {
+    //        return _disposer?.Invoke(handle) ?? true;
+    //    }
+    //}
 }
